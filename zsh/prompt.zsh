1451fd6a0f6a931fe341bbe70969469c248b8dee
eyuelt <eyueltessema@gmail.com>
2013-08-25 12:11:46 -0700

    Make multiline prompt that handles resize well

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,1 +1,12 @@
-export PROMPT='%B%n%b[%U%c%u]%# '
+PROMPTLINE1='%B%n%b[%U%c%u]%#'
+PROMPTLINE2='> '
+
+precmd() {
+  # The '\e[K' sequence erases to the end of the line. This keeps precmd
+  # from outputting extraneous spaces, which cause wrapping issues
+  print -n "\e[K\n";
+
+  print -P $PROMPTLINE1;
+}
+
+export PROMPT=$PROMPTLINE2
