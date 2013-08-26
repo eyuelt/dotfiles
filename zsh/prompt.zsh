e1553b6ef5dc1830da3d1b15fd31cfb8e8d4e011
eyuelt <eyueltessema@gmail.com>
2013-08-25 22:50:31 -0700

    Add git repo info to prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,5 +1,34 @@
-PROMPTLINE1='%B%n%b[%U%c%u]%#'
-PROMPTLINE2='> '
+git_repo_info() {
+  git rev-parse --git-dir 1>/dev/null 2>/dev/null
+  if [[ $? == 0 ]]
+  then
+    echo -n " on "
+    echo -n "$(git symbolic-ref --short HEAD 2>/dev/null)" || echo -n "(detached HEAD)"
+
+    changes=$(git status --short)
+    if [[ -z $changes ]]
+    then
+      echo -n "."
+    else
+      if [[ -n "$(git status -s | grep '^[^? ]')"  ]]  #there are staged changes
+      then
+        echo -n "⇡"
+      fi
+      if [[ -n "$(git status -s | grep '^.[^? ]')" ]]  #there are unstaged changes
+      then
+        echo -n "⇣"
+      fi
+      if [[ -n "$(git status -s | grep '^??')" ]]      #there are untracked files
+      then
+        echo -n "?"
+      fi
+    fi
+  fi
+}
+
+
+export PROMPTLINE1='%B%n%b in %U%c%u$(git_repo_info)'
+export PROMPTLINE2='> '
 
 precmd() {
   # The '\e[K' sequence erases to the end of the line. This keeps precmd
