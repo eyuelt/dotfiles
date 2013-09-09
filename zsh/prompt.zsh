e7e35b36b28abcd0fc2b81d5edd4ee81cc25502a
eyuelt <eyueltessema@gmail.com>
2013-09-09 01:13:27 -0700

    Colorize prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,10 +1,30 @@
+C0="\033[30m"
+C1="\033[31m"
+C2="\033[32m"
+C3="\033[33m"
+C4="\033[34m"
+C5="\033[35m"
+C6="\033[36m"
+C7="\033[37m"
+CRESET="\033[m"
+
+username() {
+  echo -n "$C3%n$CRESET"
+}
+
+directory() {
+  echo -n " in "
+  echo -n "$C5%U%~%u$CRESET"
+}
+
 git_repo_info() {
   git rev-parse --git-dir 1>/dev/null 2>/dev/null
   if [[ $? == 0 ]]
   then
     echo -n " on "
-    echo -n "$(git symbolic-ref --short HEAD 2>/dev/null)" || echo -n "(detached HEAD)"
+    echo -n "$C2$(git symbolic-ref --short HEAD 2>/dev/null)" || echo -n "(detached HEAD)$CRESET"
 
+    echo -n "$C5"
     changes=$(git status --short)
     if [[ -z $changes ]]
     then
@@ -23,11 +43,12 @@ git_repo_info() {
         echo -n "?"
       fi
     fi
+    echo -n "$CRESET"
   fi
 }
 
 
-export PROMPTLINE1='%B%n%b in %U%~%u$(git_repo_info)'
+export PROMPTLINE1='$(username)$(directory)$(git_repo_info)'
 export PROMPTLINE2='> '
 
 precmd() {
