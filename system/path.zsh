ea2c77fe1a90a73c54a151397bb996014639dc46
eyuelt <eyueltessema@gmail.com>
2013-10-10 22:17:39 -0700

    Add brew python directory to PYTHONPATH to be able to use python packages installed by brew

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
 export PATH=./bin:$DOTFILES/bin:$PATH
 
-export PYTHONPATH=$PYTHONPATH:/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages
+export PYTHONPATH=$PYTHONPATH:/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages:$(brew --prefix)/lib/python2.7/site-packages
