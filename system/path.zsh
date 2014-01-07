b65da01298f11c787deaf08845f6bc3b7300eee4
eyuelt <eyueltessema@gmail.com>
2014-01-07 14:00:11 -0800

    Fix PYTHONPATH for when brew is not installed

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,8 @@
 export PATH=./bin:$DOTFILES/bin:$PATH
 
-export PYTHONPATH=$PYTHONPATH:/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages:$(brew --prefix)/lib/python2.7/site-packages
+PYTHONPATH=$PYTHONPATH:/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages
+if [[ -a $(which brew) ]]
+then
+  PYTHONPATH=$PYTHONPATH:$(brew --prefix)/lib/python2.7/site-packages
+fi
+export PYTHONPATH
