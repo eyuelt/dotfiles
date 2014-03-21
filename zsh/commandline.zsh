97326305dd81ca8c49e4113ffc8d3f9c25c00519
eyuelt <eyueltessema@gmail.com>
2014-03-21 05:10:12 -0700

    Add Linux key bindings

--- zsh/commandline.zsh
+++ zsh/commandline.zsh
@@ -1,7 +1,14 @@
 # use up/down keys on the command line to do <Ctrl-R>-style reverse-i-search
 # through history using the substring before the cursor on the current line
+
 if [[ "$(uname -s)" == "Darwin" ]]
 then
   bindkey "^[[A" history-beginning-search-backward
   bindkey "^[[B" history-beginning-search-forward
+elif [[ "$(uname -s)" == "Linux" ]]
+then
+  # Note: having a .zshenv file with DEBIAN_PREVENT_KEYBOARD_CHANGES set
+  # would prevent the need for a different case on Debian machines
+  bindkey "^[OA" history-beginning-search-backward
+  bindkey "^[OB" history-beginning-search-forward
 fi
