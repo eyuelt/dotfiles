72dde613bfafc81c9a54dc42076152a6755c2c34
eyuelt <eyueltessema@gmail.com>
2014-03-21 04:50:22 -0700

    Check OS before doing key binding

--- zsh/commandline.zsh
+++ zsh/commandline.zsh
@@ -1,4 +1,7 @@
 # use up/down keys on the command line to do <Ctrl-R>-style reverse-i-search
 # through history using the substring before the cursor on the current line
-bindkey "^[[A" history-beginning-search-backward
-bindkey "^[[B" history-beginning-search-forward
+if [[ "$(uname -s)" == "Darwin" ]]
+then
+  bindkey "^[[A" history-beginning-search-backward
+  bindkey "^[[B" history-beginning-search-forward
+fi
