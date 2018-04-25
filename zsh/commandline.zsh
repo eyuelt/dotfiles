7f960b4769eafc3324b194367d5e5663ae4193fb
Eyuel Tessema <eyueltessema@gmail.com>
2018-04-25 07:29:00 -0700

    Remove the separate case for non-Mac devices as it seems not to be needed anymore. Also add Ctrl-Left/Right to jump from word to word.

--- zsh/commandline.zsh
+++ zsh/commandline.zsh
@@ -1,14 +1,8 @@
 # use up/down keys on the command line to do <Ctrl-R>-style reverse-i-search
 # through history using the substring before the cursor on the current line
+bindkey "^[[A" history-beginning-search-backward
+bindkey "^[[B" history-beginning-search-forward
 
-if [[ "$(uname -s)" == "Darwin" ]]
-then
-  bindkey "^[[A" history-beginning-search-backward
-  bindkey "^[[B" history-beginning-search-forward
-elif [[ "$(uname -s)" == "Linux" ]]
-then
-  # Note: having a .zshenv file with DEBIAN_PREVENT_KEYBOARD_CHANGES set
-  # would prevent the need for a different case on Debian machines
-  bindkey "^[OA" history-beginning-search-backward
-  bindkey "^[OB" history-beginning-search-forward
-fi
+# use <Ctrl-Left> and <Ctrl-Right> to jump from word to word
+bindkey '^[[1;5D' backward-word
+bindkey '^[[1;5C' forward-word
