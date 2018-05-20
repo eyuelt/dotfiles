8a096fe62b8ceba58c32af698e0129ef5636e185
Eyuel Tessema <eyueltessema@gmail.com>
2018-05-20 00:48:30 -0700

    Change prompt.zsh function names to specify scope

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -8,30 +8,30 @@ C6="\033[36m"
 C7="\033[37m"
 CRESET="\033[m"
 
-username() {
+prompt_username() {
   echo -n "$C3%n$CRESET"
 }
 
-computer_name() {
+prompt_computer_name() {
   if [[ -n $NICKNAME ]]
   then
     echo -n "@$C1$NICKNAME$CRESET"
   fi
 }
 
-screen_info() {
+prompt_screen_info() {
   if [[ -n $STY ]]
   then
     echo -n "[$C6$STY$CRESET]"
   fi
 }
 
-directory() {
+prompt_directory() {
   echo -n " in "
   echo -n "$C5%U%~%u$CRESET"
 }
 
-git_repo_info() {
+prompt_git_repo_info() {
   inworkingtree="$(git rev-parse --is-inside-work-tree 2>/dev/null)"
   if [[ $? == 0 && $inworkingtree == 'true' ]]
   then
@@ -62,7 +62,7 @@ git_repo_info() {
 }
 
 # display a symbol if x-forwarding is enabled
-x_symbol() {
+prompt_x_symbol() {
     if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]
     then
         if [ -n "$DISPLAY" ]
@@ -72,15 +72,17 @@ x_symbol() {
     fi
 }
 
-export PROMPTLINE1='┌─$(x_symbol) $(username)$(computer_name)$(screen_info)$(directory)$(git_repo_info)'
-export PROMPTLINE2='└> '
+prompt_line_1='┌─$(prompt_x_symbol) $(prompt_username)$(prompt_computer_name)$(prompt_screen_info)$(prompt_directory)$(prompt_git_repo_info)'
+prompt_line_2='└> '
 
+# This function is executed by zsh before each prompt.
 precmd() {
   # The '\e[K' sequence erases to the end of the line. This keeps precmd
   # from outputting extraneous spaces, which cause wrapping issues
   print -n "\e[K\n";
 
-  print -P $PROMPTLINE1;
+  print -P $prompt_line_1;
 }
 
-export PROMPT=$PROMPTLINE2
+# This envvar defines the zsh prompt.
+export PROMPT=$prompt_line_2
