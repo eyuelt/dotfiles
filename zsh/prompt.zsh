221dd0b0434419eca9739f05c7b7be978f3ae71b
Eyuel Tessema <eyueltessema@gmail.com>
2019-06-02 00:07:32 -0700

    Add support for rprompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,3 +1,12 @@
+# Note: It is important that PROMPT is short because when the window is resized
+# to be shorter than the length of PROMPT, weird things happen (mainly the lines
+# above the prompt get overwritten and the prompt gets duplicated). We avoid
+# this issue by printing most of the prompt info via precmd instead of using the
+# PROMPT variable.
+
+# This script uses ANSI escape codes (https://en.wikipedia.org/wiki/ANSI_escape_code).
+# See http://ascii-table.com/ansi-escape-sequences-vt-100.php for a list of ANSI escape codes.
+
 C0="\033[30m"
 C1="\033[31m"
 C2="\033[32m"
@@ -72,17 +81,39 @@ prompt_x_symbol() {
     fi
 }
 
+# This function displays a right-side prompt that is level with the first line
+# of the prompt. This is different from the built-in RPROMPT var, which writes
+# on the last line of the prompt (the same line as the command). This avoids the
+# redraw issues on Linux when resizing the window when RPROMPT is set.
+display_rprompt() {
+  rprompt_len="${#$(strip_ansi_escape_sequences ${rprompt})}"
+  print -n "\e7"                     # saves the cursor position
+  print -n "\e[1000C"                # moves the cursor to the right 1000 times (or until it reaches the right side of the screen) TODO: use $COLUMNS
+  print -n "\e[${rprompt_len}D"      # moves the cursor back to the left <length_of_rprompt> times
+  print -n "${rprompt}"              # prints out $rprompt
+  print -n "\e8"                     # restores the original cursor position
+}
+
 prompt_line_1='┌─$(prompt_x_symbol) $(prompt_username)$(prompt_computer_name)$(prompt_screen_info)$(prompt_directory)$(prompt_git_repo_info)'
 prompt_line_2='└> '
 
+rprompt=''
+
 # This function is executed by zsh before each prompt.
 precmd() {
-  # The '\e[K' sequence erases to the end of the line. This keeps precmd
-  # from outputting extraneous spaces, which cause wrapping issues
+  # The '\e[K' sequence erases to the end of the line. This keeps precmd from
+  # outputting extraneous spaces, which cause wrapping issues
   print -n "\e[K\n";
 
-  print -P $prompt_line_1;
+  print -nP $prompt_line_1;
+
+  if [[ -n $rprompt ]]
+  then
+    display_rprompt
+  fi
+
+  print -n "\n"
 }
 
-# This envvar defines the zsh prompt.
+# This envvar defines the actual zsh prompt.
 export PROMPT=$prompt_line_2
