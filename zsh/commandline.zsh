98b4a212798b543257ab8a0ce93bd3ae06361a83
Eyuel Tessema <eyueltessema@gmail.com>
2020-05-08 10:18:12 +0100

    Small fix

--- zsh/commandline.zsh
+++ zsh/commandline.zsh
@@ -1,7 +1,12 @@
 # use up/down keys on the command line to do <Ctrl-R>-style reverse-i-search
 # through history using the substring before the cursor on the current line
-bindkey "^[[A" history-beginning-search-backward
-bindkey "^[[B" history-beginning-search-forward
+# Note: whether to use \e[A or \eOA varies by machine (e.g. I had to do \e[A on
+# an Ubuntu machine, while I had to do \eOA on a RPi running Raspbian), so we
+# just define both here.
+bindkey "\e[A" history-beginning-search-backward
+bindkey "\eOA" history-beginning-search-backward
+bindkey "\e[B" history-beginning-search-forward
+bindkey "\eOB" history-beginning-search-forward
 
 # use <Ctrl-Left> and <Ctrl-Right> to jump from word to word
 bindkey '^[[1;5D' backward-word
